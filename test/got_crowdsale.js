355b5d75287f5b73b63d1c809b7f637ffdda2954
carpediemsrl <simone.manneschi@gmail.com>
2018-05-24 10:07:52 +0200

    fix depending on eidoo test sample

--- test/got_crowdsale.js
+++ test/got_crowdsale.js
@@ -14,52 +14,41 @@ const should = require('chai') // eslint-disable-line
     .should();
 
 // Values for testing buy methods with the required MAX_AMOUNT by Eidoo's KYCBase contract
-const SIGNER_PK = Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex');
-const MAX_AMOUNT = '1000000000000000000';
+const SIGNER_PK = Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex')
+const SIGNER_ADDR = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57'.toLowerCase()
+const OTHER_PK = Buffer.from('0dbbe8e4ae425a6d2687f1a7e3ba17bc98c673636790f1b8ad91193c05875ef1', 'hex')
+const OTHER_ADDR = '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef'.toLowerCase()
+const MAX_AMOUNT = '1000000000000000000'
 
 const getKycData = (userAddr, userid, icoAddr, pk) => {
-    // sha256("Eidoo icoengine authorization", icoAddress, buyerAddress, buyerId, maxAmount);
-    const hash = abi.soliditySHA256(
-        ['string', 'address', 'address', 'uint64', 'uint'],
-        ['Eidoo icoengine authorization', icoAddr, userAddr, new BN(userid), new BN(MAX_AMOUNT)]
-    );
-    const sig = ecsign(hash, pk);
-    return {
-        id: userid,
-        max: MAX_AMOUNT,
-        v: sig.v,
-        r: '0x' + sig.r.toString('hex'),
-        s: '0x' + sig.s.toString('hex')
-    };
-};
-
-// Values with higher MAX_AMOUNT to test when cap has been reached
-const MAX_AMOUNT_MOCK = '60001000000000000000000';
-
-const getKycDataMockMaxAmount = (userAddr, userid, icoAddr, pk) => {
-    // sha256("Eidoo icoengine authorization", icoAddress, buyerAddress, buyerId);
-    const hash = abi.soliditySHA256(
-        ['string', 'address', 'address', 'uint64', 'uint'],
-        ['Eidoo icoengine authorization', icoAddr, userAddr, new BN(userid), new BN(MAX_AMOUNT_MOCK)]
-    );
-    const sig = ecsign(hash, pk);
-    return {
-        id: userid,
-        max: MAX_AMOUNT_MOCK,
-        v: sig.v,
-        r: '0x' + sig.r.toString('hex'),
-        s: '0x' + sig.s.toString('hex')
-    };
-};
+  // sha256("Eidoo icoengine authorization", icoAddress, buyerAddress, buyerId, maxAmount);
+  const hash = abi.soliditySHA256(
+    [ 'string', 'address', 'address', 'uint64', 'uint' ],
+    [ 'Eidoo icoengine authorization', icoAddr, userAddr, new BN(userid), new BN(MAX_AMOUNT) ]
+  )
+  const sig = ecsign(hash, pk)
+  return {
+    id: userid,
+    max: MAX_AMOUNT,
+    v: sig.v,
+    r: '0x' + sig.r.toString('hex'),
+    s: '0x' + sig.s.toString('hex')
+  }
+}
 
 const USD_PER_TOKEN = 1;
 const USD_PER_ETHER = 700;
-const TOKEN_PER_ETHER =  USD_PER_ETHER / USD_PER_TOKEN;                     // 250 UAC tokens per ether
+const TOKEN_PER_ETHER =  USD_PER_ETHER / USD_PER_TOKEN;                     // 700 GOT tokens per ether
+
+/*INVESTORS DATA*/
+// First investment: activeInvestor1
+const INVESTOR1_WEI = 1e18;
+const INVESTOR1_TOKEN_AMOUNT = 270 * 1e18;
 
 
 contract('GotCrowdSale',(accounts) => {
     const owner = accounts[0];
-    const activeInvestor = accounts[1];
+    const activeInvestor1 = accounts[1];
 
   // Provide gotTokenInstance for every test case
   let gotTokenInstance;
@@ -83,6 +72,11 @@ contract('GotCrowdSale',(accounts) => {
     it('should have token ownership', async () => {
         const gotTokenInstanceOwner = await gotTokenInstance.owner();
         gotTokenInstanceOwner.should.equal(gotCrowdSaleInstance.address);
+    });
+
+    it('should fail, buyTokens method can not be called before crowdsale phase starts', async () => {
+        const d = getKycData(activeInvestor1, 1, gotCrowdSaleInstance.address, SIGNER_PK);
+        await expectThrow(gotCrowdSaleInstance.buyTokens(d.id, d.max, d.v, d.r, d.s, {from: activeInvestor1, value: INVESTOR1_WEI}));
     });
 
 });

52057b53366ddc8a57e9ca35fa76ee04ebc7e561
Matt Swezey <matt@mattswezey.net>
2018-06-04 14:42:51 -0500

    Update Tooling

---
+++ .solcover.js
@@ -0,0 +1,8 @@
+module.exports = {
+    port: 8555,
+    copyNodeModules: false,
+    testrpcOptions: '--port 8555 --defaultBalanceEther 1000000', // --defaultBalanceEther: Amount of ether to assign each test account. Default is 100.
+    copyPackages: ['openzeppelin-solidity'],
+    norpc: false,
+    skipFiles: ['']
+};
